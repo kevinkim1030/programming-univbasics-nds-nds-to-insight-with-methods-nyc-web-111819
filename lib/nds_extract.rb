cddc970a04a293de88d12189e89eb1613c8751a2
Kevin Kim <kevin.kim1030@gmail.com>
2019-11-14 23:27:11 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -30,7 +30,9 @@ end
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
-  
+  total = 0 
+  gross_index = 0 
+  while gross_index < director_data[]
   
   binding.pry
 end
